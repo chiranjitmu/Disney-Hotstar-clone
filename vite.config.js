09f70e47ff6369d97bfd3b714a16e2f34feec136
chiranjitmu <chiranjitmukherjee071@gmail.com>
2026-07-02 11:15:51 +0530

    env bug fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  envPrefix: 'REACT_APP_',
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.jsx?$/,
